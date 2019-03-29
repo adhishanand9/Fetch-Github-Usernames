b7600e809583911a282d194c39acebd759fb688f
Adhish Anand <adhishanand9@gmail.com>
2019-03-29 15:35:58 +0530

    [Fetch Github Details using Id or Name]

---
+++ Script.js
@@ -0,0 +1,54 @@
+var btn=document.getElementById('but');
+var input = document.getElementById("inp");
+var tbody = document.getElementById("parent");
+var value;
+btn.addEventListener("click",function(){
+  value = input.value;
+  if(!value)
+	{
+	   alert("No Value Entered.");
+		 return;
+	}
+  getUser(value);
+});
+var user=[];
+function getUser(val){
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://api.github.com/users?since="+val);
+  xhr.onload = function(){
+    //console.log(xhr.responseText);
+    var array=xhr.responseText;
+    user=JSON.parse(array);
+    console.log(user[0]);
+  };
+  xhr.send();
+  generateRows();
+}
+var i=0;
+function  generateRows()
+{
+      console.log(i);
+      var row=document.createElement('tr');
+      row.setAttribute('id',i);
+      var col1=document.createElement('td');
+      var img=document.createElement("IMG");
+      img.src=user[i].avatar_url;
+      img.width=90;
+      img.height=100;
+      col1.appendChild(img);
+      row.appendChild(col1);
+
+      var col1=document.createElement('td');
+      col1.innerHTML=user[i].id;
+      row.appendChild(col1);
+
+      var col1=document.createElement('td');
+      col1.innerHTML=user[i].login;
+      row.appendChild(col1);
+
+      var col1=document.createElement('td');
+      col1.innerHTML=user[i].html_url;
+      row.appendChild(col1);
+      tbody.appendChild(row);
+      i++;
+}
---
+++ ScriptName.js
@@ -0,0 +1,51 @@
+var btn=document.getElementById('but');
+var input = document.getElementById("inp");
+var tbody = document.getElementById("parent");
+var val1;
+btn.addEventListener("click",function(){
+  val1 = input.value;
+  if(!val1)
+	{
+	   alert("No Value Entered.");
+		 return;
+	}
+  getUser(val1);
+});
+var user;
+function getUser(val){
+  console.log(val);
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://api.github.com/users/"+val,true);
+  xhr.onload = function(){
+    var array=xhr.responseText;
+    console.log(array);
+    user=JSON.parse(array);
+    generateRows();
+  };
+  xhr.send();
+}
+function  generateRows()
+{
+      var row=document.createElement('tr');
+      var col1=document.createElement('td');
+      var img=document.createElement("IMG");
+      img.src=user.avatar_url;
+      img.width=90;
+      img.height=100;
+      col1.appendChild(img);
+      row.appendChild(col1);
+
+      var col1=document.createElement('td');
+      col1.innerHTML=user.id;
+      row.appendChild(col1);
+
+      var col1=document.createElement('td');
+      col1.innerHTML=user.login;
+      row.appendChild(col1);
+
+      var col1=document.createElement('td');
+      col1.innerHTML=user.html_url;
+      row.appendChild(col1);
+      tbody.appendChild(row);
+
+}
